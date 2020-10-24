6ea00b46c5254352e4afe47758eca355fed89ab3
karan1206 <64606159+karanjoshi1206@users.noreply.github.com>
2020-10-24 21:25:07 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,32 +1,41 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const path = require("path")
-const port = process.env.PORT || 3000
-const hbs = require("hbs");
+const hbs = require('hbs');
+const port = process.env.PORT || 8000;
 
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
-const staticpath = path.join(__dirname, "../public")
-const partialpath = path.join(__dirname, "../partials")
+app.set('view engine', 'hbs');
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
 
-app.set("view engine", "hbs")
+// console.log(static_path);
+app.use(express.static(static_path));
 
-
-hbs.registerPartials(partialpath)
-app.use(express.static(staticpath))
-
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("", (req, res) => {
+    res.render('index')
 })
+
 app.get("/about", (req, res) => {
-    res.render("about")
+    res.render('about')
 })
+
+
 app.get("/weather", (req, res) => {
-    res.render("weather")
+    res.render('weather')
 })
+
+
 app.get("*", (req, res) => {
-    res.render("404")
+    res.render('404page', {
+        errorMsg: "Opps! page not found, Click Here to go"
+    })
 })
+
+
 app.listen(port, () => {
-    console.log("listening")
-}
-)
+    console.log(`listening to the port no at ${port}`);
+})
